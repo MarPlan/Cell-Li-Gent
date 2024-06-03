8d49b9698768db0af206a7afe5a66c885b256385
MarPlan <93042964+MarPlan@users.noreply.github.com>
2024-06-03 20:36:24 +0200

    simple surface submodel for data generation
    surface temperature is very simple, caputres a different time constant and amplitude vs. the core temperature of the battery cell

--- model/dfn.py
+++ model/dfn.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pybamm
 
@@ -49,19 +48,24 @@ class Lumped(pybamm.thermal.base_thermal.BaseThermal):
         return variables
 
     def get_coupled_variables(self, variables):
-        # T_surf = variables["Volume-averaged surface temperature [K]"]
-        # T_dict = {"Volume-averaged surface temperature [K]": T_surf}
+        T_surf = variables["Volume-averaged surface temperature [K]"]
+        T_dict = {"Volume-averaged surface temperature [K]": T_surf}
         variables.update(self._get_standard_coupled_variables(variables))
-
+        variables.update(T_dict)
         return variables
 
     def set_rhs(self, variables):
+        """
+        Simple attempt for assuming only thermal conduction between cell and surface:
+        in theory: R_cond ~ d/(kA) and R_conv ~ 1/(hA)
+        here h_fraction represents only a very simplified case
+        """
         T_vol_av = variables["Volume-averaged cell temperature [K]"]
         Q_vol_av = variables["Volume-averaged total heating [W.m-3]"]
         T_surf = variables["Volume-averaged surface temperature [K]"]
 
         # Newton cooling, accounting for surface area to volume ratio
-        h_fraction = 100
+        h_fraction = 3
         cell_surface_area = self.param.A_cooling
         cell_volume = self.param.V_cell
         Q_cool_vol_av = (
@@ -81,65 +85,26 @@ class Lumped(pybamm.thermal.base_thermal.BaseThermal):
         self.initial_conditions = {T_vol_av: self.param.T_init}
 
 
-class LumpedSurface(pybamm.thermal.base_thermal.BaseThermal):
+class LumpedSurface(Lumped):
     """
-    Class for lumped thermal submodel. For more information see :footcite:t:`Timms2021`
-    and :footcite:t:`Marquis2020`.
-
-    Parameters
-    ----------
-    param : parameter class
-        The parameters to use for this submodel
-    options : dict, optional
-        A dictionary of options to be passed to the model.
-
+    Not coupling heat sources from core and surface directly to realize a different
+    time constant, not very realistic but perhaps good enough for a fast evaluation
     """
 
     def __init__(self, param, options=None):
         super().__init__(param, options=options)
         pybamm.citations.register("Timms2021")
 
-    def get_fundamental_variables(self):
-        T_vol_av = pybamm.Variable(
-            "Volume-averaged cell temperature [K]",
-            scale=self.param.T_ref,
-            print_name="T_av",
-        )
-        T_x_av = pybamm.PrimaryBroadcast(T_vol_av, ["current collector"])
-
-        T_surf = pybamm.Variable(
-            "Volume-averaged surface temperature [K]",
-            scale=self.param.T_ref,
-            print_name="T_surf",
-        )
-
-        T_dict = {
-            "volume-averaged surface": T_surf,
-            "negative current collector": T_x_av,
-            "positive current collector": T_x_av,
-            "x-averaged cell": T_x_av,
-            "volume-averaged cell": T_vol_av,
-        }
-        for domain in ["negative electrode", "separator", "positive electrode"]:
-            T_dict[domain] = pybamm.PrimaryBroadcast(T_x_av, domain)
-
-        variables = self._get_standard_fundamental_variables(T_dict)
-
-        return variables
-
-    def get_coupled_variables(self, variables):
-        variables.update(self._get_standard_coupled_variables(variables))
-        return variables
-
     def set_rhs(self, variables):
         T_surf = variables["Volume-averaged surface temperature [K]"]
-        # T_vol_av = variables["Volume-averaged cell temperature [K]"]
         Q_vol_av = variables["Volume-averaged total heating [W.m-3]"]
         T_amb = variables["Volume-averaged ambient temperature [K]"]
+        T_vol_av = variables["Volume-averaged cell temperature [K]"]
 
         # Newton cooling, accounting for surface area to volume ratio
         cell_surface_area = self.param.A_cooling
         cell_volume = self.param.V_cell
+
         Q_cool_vol_av = (
             -self.param.h_total * (T_surf - T_amb) * cell_surface_area / cell_volume
         )
@@ -152,26 +117,46 @@ class LumpedSurface(pybamm.thermal.base_thermal.BaseThermal):
 
 
 if __name__ == "__main__":
-    t = np.linspace(0, 900, 60)
-    sin_t = 9.5 * np.sin(2 * np.pi * t)
-    drive_cycle_power = np.column_stack([t, sin_t])
+    import os
+    import sys
+
+    import matplotlib.pyplot as plt
+
+    sys.path.append(os.path.abspath(".."))
+    from util.config_data import BatteryDatasheet
+
+    os.chdir("..")
+    specs = BatteryDatasheet()
+    curr = np.load("data/current/test_dfn.npy", allow_pickle=True)[0, :239_000]
+    t = np.arange(0, curr.shape[0], specs.dt)
+    cycle = np.column_stack([t, curr])
+    current_interpolant = pybamm.Interpolant(cycle[:, 0], cycle[:, 1], pybamm.t)
 
     parameter_values = pybamm.ParameterValues("Chen2020")
+    parameter_values["Total heat transfer coefficient [W.m-2.K-1]"] = 2
+    parameter_values["Current function [A]"] = current_interpolant
     options = {"cell geometry": "arbitrary"}
     model = pybamm.lithium_ion.DFN(options, build=False)
-
     model.submodels["thermal"] = Lumped(model.param, options)
     model.submodels["thermal_surf"] = LumpedSurface(model.param, options)
     model.build_model()
 
-    experiment = pybamm.Experiment([pybamm.step.current(drive_cycle_power)])
+    # TODO: TQMD for simulation
     sim = pybamm.Simulation(
-        model, parameter_values=parameter_values, experiment=experiment
+        model,
+        parameter_values=parameter_values,
+        solver=pybamm.CasadiSolver(
+            mode="fast", extra_options_setup={"max_step_size": 1}
+        ),
     )
     solution = sim.solve()
+
+    solution.save("data/profile_0.pkl")
+
     outputs = [
         "Current [A]",
         "Terminal voltage [V]",
+        "Discharge capacity [A.h]",
         "X-averaged cell temperature [C]",
         "Volume-averaged surface temperature [C]",
     ]
